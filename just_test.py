7ad354177550ae0adf63b68cc705ced0f4591894
n1nekto <ad.moxito@mail.ru>
2021-09-29 11:53:27 +0300

    home

--- just_test.py
+++ just_test.py
@@ -1,1 +1,26 @@
-print('hello')
+# a = list()
+# b = tuple()
+# c = dict()
+# d = set()
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+for i in range(0,50):
+	print(i, end=',')
+
+for i in range(0,50,2):
+	print(i, end=',')
+
+
+first_while = 0
+while first_while < 51:
+	print(first_while, end=' ')
+	first_while += 1
+print('')
+
+second_while = 0
+while second_while < 51:
+	print(second_while, end=' ')
+	second_while += 2
